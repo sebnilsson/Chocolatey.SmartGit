916042b324b8b5d43067a52f8a01aef0f301f867
Bob Tanner <tanner@real-time.com>
2018-06-16 21:00:12 -0500

    Closes #4

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,16 +1,16 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$version = '17_1_3'
-$fileName = "smartgit-win32-setup-jre-$version.zip"
+$version = '18_1_3'
+$fileName = "smartgit-win-$version.zip"
 $packageArgs = @{
     packageName    = 'SmartGit'
     fileType       = 'exe'
     softwareName   = 'SmartGit'
     unzipLocation  = Get-PackageCacheLocation
 
-    checksum       = 'A3DA0E8B86195F749BBD895405FE35C03EA9E658'
-    checksumType   = 'sha1'
-    url            = "https://www.syntevo.com/static/smart/download/smartgit/$fileName"
+    checksum       = '40b7fff890b778a219a402bbc3cfaea1a8c170aedcc2107099a41b9cfcae1395'
+    checksumType   = 'sha256'
+    url            = "https://www.syntevo.com/downloads/smartgit/$fileName"
 
     silentArgs     = '/SILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
     validExitCodes = @(0)
@@ -19,7 +19,7 @@ $packageArgs = @{
 Write-Verbose "Testing the default url location."
 if ((Get-WebHeaders $packageArgs.url).'Content-Type' -match "^text/html" ) {
   Write-Warning "File not found, trying to find it in the archive."
-  $packageArgs.url = "https://www.syntevo.com/static/smart/download/smartgithg/archive/$fileName"
+  $packageArgs.url = "https://www.syntevo.com/downloads/smartgit/$fileName"
 }
 
 Install-ChocolateyZipPackage @packageArgs
@@ -27,5 +27,3 @@ Install-ChocolateyZipPackage @packageArgs
 $packageArgs.file = (Join-Path $packageArgs.unzipLocation (Get-ChildItem $packageArgs.unzipLocation -Filter '*.exe' | select -first 1))
 
 Install-ChocolateyInstallPackage @packageArgs
-
-Uninstall-ChocolateyZipPackage -PackageName $packageArgs.packageName -ZipFileName $fileName

b1affe0c1a070bced60c47ff107321338dfbc91e
Seb Nilsson <seb.nilsson@gmail.com>
2019-09-27 11:18:09 +0200

    SmartGit 19.1.3

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,29 +1,30 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$version = '19_1_1'
-$fileName = "smartgit-win-$version.zip"
-$packageArgs = @{
-    packageName    = 'SmartGit'
-    fileType       = 'exe'
-    softwareName   = 'SmartGit'
-    unzipLocation  = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+$version = '19_1_3'
 
-    checksum       = 'EAC3686BD17D4BAE9C147F59966BF415D936AC8724FFB7403DDDFAB6B0C10120'
+$packageName = 'SmartGit'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileName = "smartgit-win-$version.zip"
+$url = "https://www.syntevo.com/downloads/smartgit/$fileName"
+$archiveUrl = "https://www.syntevo.com/downloads/smartgit/archive/$fileName"
+
+$packageArgs = @{
+    packageName    = $packageName
+    unzipLocation  = $toolsDir
+    url            = $url
+
+    checksum       = '62F5C2C3646B59E667EE78CA93CFD2F9325084900BC03B87DC0C3ABE236A1FA4'
     checksumType   = 'sha256'
-    url            = "https://www.syntevo.com/downloads/smartgit/$fileName"
 
     silentArgs     = '/SILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
     validExitCodes = @(0)
 }
 
 Write-Verbose "Testing the default url location."
+
 if ((Get-WebHeaders $packageArgs.url).'Content-Type' -match "^text/html" ) {
   Write-Warning "File not found, trying to find it in the archive."
-  $packageArgs.url = "https://www.syntevo.com/downloads/smartgit/archive/$fileName"
+  $packageArgs.url = $archiveUrl
 }
 
 Install-ChocolateyZipPackage @packageArgs
-
-$packageArgs.file = (Join-Path $packageArgs.unzipLocation (Get-ChildItem $packageArgs.unzipLocation -Filter '*.exe' | select -first 1))
-
-Install-ChocolateyInstallPackage @packageArgs

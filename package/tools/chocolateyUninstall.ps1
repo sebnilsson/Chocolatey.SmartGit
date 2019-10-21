75bd295c6611291bba16e0c8d3db5b517450707b
Seb Nilsson <seb.nilsson@gmail.com>
2019-10-21 16:05:57 +0200

    Moved package-files to avoid packing git-related files

---
+++ package/tools/chocolateyUninstall.ps1
@@ -0,0 +1,14 @@
+﻿$packageName = "SmartGit"
+$installerType = "exe"
+
+$folderName = "SmartGit"
+$uninstallFile = "unins000.exe"
+
+# Uninstall PDFCreator if older version is installed
+if (Test-Path "$env:ProgramFiles\$folderName") {
+    Uninstall-ChocolateyPackage $packageName $installerType "/VERYSILENT /NORESTART" "$env:ProgramFiles\$folderName\$uninstallFile"
+}
+
+if (Test-Path "${env:ProgramFiles(x86)}\$folderName") {
+    Uninstall-ChocolateyPackage $packageName $installerType "/VERYSILENT /NORESTART" "${env:ProgramFiles(x86)}\$folderName\$uninstallFile"
+}

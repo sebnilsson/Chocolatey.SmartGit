8a0e6e01d2f41d5f0a14c2034a7733528bb3c8fc
Mike Schwörer <pubgit@mikescher.com>
2020-05-20 13:18:23 +0200

    SmartGit 20.1.2

--- package/tools/chocolateyInstall.ps1
+++ package/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$version = '20_1_1'
+$version = '20_1_2'
 
 $packageName = 'SmartGit'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
@@ -12,10 +12,10 @@ $file = Join-Path $toolsDir "smartgit-$version-setup.exe"
 $packageArgs = @{
     packageName    = $packageName
     unzipLocation  = $toolsDir
-    url64            = $url
+    url64          = $url
 
-    checksum64       = 'DB1472C5982C5659E43A93269EE9A6641E545C333767D2B432AD4839FB4F1CCC'
-    checksumType64   = 'sha256'
+    checksum64     = '30AB1489BCDE76B966615F16DBB0564A45935266FDB93B84DCAB6B5EDDF99EFB'
+    checksumType64 = 'sha256'
 }
 
 $installArgs = @{

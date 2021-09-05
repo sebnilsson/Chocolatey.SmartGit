6d8e51ef99995d687b367280b51de737286d4c0e
Noah Sherwin <noahrsherwin@gmail.com>
2021-09-05 02:44:19 -0700

    Support uninstalling custom paths; bump SmartGit to 21.1.1 (#9)
    * doc: change 'PDFCreator' to 'SmartGit'
    
    * feat: Add uninstall support for custom paths
    
    * chore: Bump SmartGit version and hash to 21.1.1
    
    * doc: Add GitHub Workflow To-Do
    
    * Nuspec: 21.1.1
    
    Co-authored-by: Seb Nilsson <seb.nilsson@gmail.com>

--- package/tools/chocolateyUninstall.ps1
+++ package/tools/chocolateyUninstall.ps1
@@ -4,11 +4,34 @@ $installerType = "exe"
 $folderName = "SmartGit"
 $uninstallFile = "unins000.exe"
 
-# Uninstall PDFCreator if older version is installed
-if (Test-Path "$env:ProgramFiles\$folderName") {
-    Uninstall-ChocolateyPackage $packageName $installerType "/VERYSILENT /NORESTART" "$env:ProgramFiles\$folderName\$uninstallFile"
+$key = "HKLM:\SOFTWARE\SmartGit"
+$valueName = "ApplicationIcon"
+$path = ""
+$pathExists = $false
+
+# Uninstall SmartGit if older version is installed
+try {
+    $path = (Get-ItemPropertyValue -Path $key -Name $valueName).replace("`"","")
+    $path = (Get-Item $path).Directory.Parent.FullName
+    $path = (Join-Path -Path $path -ChildPath $uninstallFile)
+    $pathExists = Test-Path $path
+}
+catch [System.NullArgumentException]{
+    $lastError = $Error[0]
+    Write-Warning -Message "Failed to get uninstaller's path from Registry.`n$lastError`nTrying default paths..."
+}
+catch [System.Management.Automation.RuntimeException] {
+    $lastError = $Error[0]
+    Write-Warning -Message "Failed to get uninstaller's path from Registry.`n$lastError`nTrying default paths..."
 }
 
-if (Test-Path "${env:ProgramFiles(x86)}\$folderName") {
+if (Test-Path "$env:ProgramFiles\$folderName") {
+    Uninstall-ChocolateyPackage $packageName $installerType "/VERYSILENT /NORESTART" "$env:ProgramFiles\$folderName\$uninstallFile"
+}elseif (Test-Path "${env:ProgramFiles(x86)}\$folderName") {
     Uninstall-ChocolateyPackage $packageName $installerType "/VERYSILENT /NORESTART" "${env:ProgramFiles(x86)}\$folderName\$uninstallFile"
+}elseif ($pathExists)
+{
+    Uninstall-ChocolateyPackage $packageName $installerType "/VERYSILENT /NORESTART" "$path"
+}else {
+    Write-Error -Message "No $packageName was found to uninstall."
 }
